4ce7f4ec079b780c14606bba26ddc6a07de9619a
ITfrontTop <128431308+ITfrontTop@users.noreply.github.com>
2025-11-17 20:22:33 +0200

    added burger menu

--- month_1/home_work_site_portfolio/js/main.js
+++ month_1/home_work_site_portfolio/js/main.js
@@ -1,3 +1,4 @@
+// кнопка запуска видео
 const videoInfo = document.querySelector('.info__video');
 const videoInfoBtn = document.querySelector('.info__video-btn');
 
@@ -5,3 +6,11 @@ function playVideo() {
   videoInfo.play();
   videoInfoBtn.style.display = 'none';
 }
+
+// кнопка бургер меню
+const btnMenu = document.querySelector('.header__burger-btn ');
+const listMenu = document.querySelector('.header__nav-inner');
+
+btnMenu.addEventListener('click', function () {
+  listMenu.classList.toggle('open');
+});
