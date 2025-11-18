dd9f42803db2e2a27d4901e46fe9f29c9066b320
ITfrontTop <128431308+ITfrontTop@users.noreply.github.com>
2025-11-18 11:05:53 +0200

    added login btn

--- month_1/home_work_site_portfolio/js/main.js
+++ month_1/home_work_site_portfolio/js/main.js
@@ -14,3 +14,11 @@ const listMenu = document.querySelector('.header__nav-inner');
 btnMenu.addEventListener('click', function () {
   listMenu.classList.toggle('open');
 });
+
+// кнопка login
+const loginBtn = document.querySelector('.login__box');
+const loginContent = document.querySelector('.settings');
+
+loginBtn.addEventListener('click', function () {
+  loginContent.classList.toggle('open');
+});
