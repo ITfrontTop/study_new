a850c0963f0cffb349d7e19abc5115bfb9a40e0a
ITfrontTop <128431308+ITfrontTop@users.noreply.github.com>
2025-11-24 15:51:11 +0200

    finished day_1

---
+++ month_2/day_1/js/script.js
@@ -0,0 +1,17 @@
+console.log(1234);
+console.log('JS Top');
+console.log(true);
+
+// Конвертер температури
+// Принимаем температуру
+const celsius = prompt('Введите температуру в Цельсиях - C');
+
+// Считаем в Фаренгейтах
+const fahrenheit = celsius * 1.8 + 32;
+
+// Робимо перевірку і виводимо температуру
+if (fahrenheit === 32 || String) {
+  alert(`Введите температуру заново`);
+} else {
+  alert(`${fahrenheit} - температура в Фаренгейтах - F`);
+}
