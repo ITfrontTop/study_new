e299d1b9fc7b5a4c70f5b3023588e9367d19dfd4
ITfrontTop <128431308+ITfrontTop@users.noreply.github.com>
2026-01-05 16:57:11 +0200

    added dark mode theme

--- day_21/Todo-App_PRO/js/main.js
+++ day_21/Todo-App_PRO/js/main.js
@@ -1,12 +1,34 @@
+// import { data } from './module/data';
+const body = document.querySelector('body');
+
+const themeText = document.querySelector('.theme__text');
+const themeBtn = document.querySelector('.theme__btn');
+
 const allBtn = document.querySelector('.all__btn');
 const doneBtn = document.querySelector('.done__btn');
 const list = document.querySelector('.list');
 const done = document.querySelector('.done');
 
 const itemText = document.querySelector('.list__item-text');
+// edit text
 itemText.contentEditable = 'true';
 
-// if we click this button added class active on .list and .all__btn
+// change theme
+let mode = 'light';
+let textMode = 'dark';
+console.log(themeText.textContent);
+themeBtn.addEventListener('click', () => {
+  body.classList.toggle('dark-mode');
+  if (textMode === 'dark') {
+    themeText.textContent = 'light';
+    textMode = 'light';
+  } else {
+    themeText.textContent = 'dark';
+    textMode = 'dark';
+  }
+});
+
+// if we click this button added class active on .all__btn and .list; .doneBtn and done
 function addActiveClass(btn, item) {
   btn.classList.add('active');
   item.classList.add('active');
@@ -26,3 +48,6 @@ doneBtn.addEventListener('click', () => {
   addActiveClass(doneBtn, done);
   removeActiveClass(allBtn, list);
 });
+
+// проверить
+// console.log(data);
